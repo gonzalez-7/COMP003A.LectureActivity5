37889e3357b81a7e408947b4a5ef23a137cb8935
gonzalez-7 <20agonzalezp@gmail.com>
2026-02-12 20:05:40 -0800

    Steps D - F

--- COMP003A.LectureActivity5/Program.cs
+++ COMP003A.LectureActivity5/Program.cs
@@ -7,12 +7,15 @@
             static void DisplayWelcome()
             {
                 Console.WriteLine("Welcome to Module 5!");
+                Console.WriteLine(mainOnlyNumber);
             }
             DisplayWelcome();
 
-            //
+			int mainOnlyNumber = 100;
 
-            static void DisplayUserInfo(string name, int age)
+			//
+
+			static void DisplayUserInfo(string name, int age)
             {
                 Console.WriteLine($"Name: {name}, Age: {age}");
             }
@@ -28,6 +31,30 @@
             {
                 return num1 + num2; 
             }
+
+            //
+
+            int sum = CalculateSum(7, 8);
+            Console.WriteLine($"The sum is: {sum}");
+
+            //
+
+            if (true)
+            {
+                int insideIf = 50;
+                Console.WriteLine($"Inside if: {insideIf}");
+            }
+
+            Console.WriteLine(insideIf);
+
+            //
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Loop counter: {i}");
+            }
+
+            Console.WriteLine(i);
         }
     }
 }
